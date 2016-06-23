3d8ba6d1188cc89e9bb43a5dde47305b1d96a3bf
Lahari <lahu.manchik@ufl.edu>
2016-06-22 20:34:12 -0400

    got countdown to 0 and then enabled break button starting at 5 minutes

--- public/js/app.js
+++ public/js/app.js
@@ -2,26 +2,39 @@ $(document).ready(function(){
   var start = $('#start');
   var minutes = $('#minutes');
   var seconds = $('#seconds');
+  var breakBtn = $('#break');
 
   start.on('click', startCountdown);
+  breakBtn.on('click', takeABreak);
+
+  function takeABreak(){
+    minutes.text('00');
+    seconds.text('02');
+    startCountdown();
+  }
 
   function startCountdown() {
     var countdown = setInterval(function(){
-    var minutesVal = +minutes.text();
-    var secondsVal = +seconds.text();
-
-    if(secondsVal === 0){
-      minutes.text(minutesVal - 1);
-      seconds.text(59);
-    }
-    else {
-      if(secondsVal <= 10){
-        seconds.text("0" +(secondsVal-1));
+      var minutesVal = +minutes.text();
+      var secondsVal = +seconds.text();
+      if(secondsVal === 0 && minutesVal ===0){
+        breakBtn.removeClass('disabled');
+        breakBtn.removeAttr('disabled');
+        clearInterval(countdown);
+        return;
       }
-      else{
-        seconds.text(secondsVal - 1);
+      if(secondsVal === 0){
+        minutes.text(minutesVal - 1);
+        seconds.text(59);
       }
-    }
+      else {
+        if(secondsVal <= 10){
+          seconds.text("0" +(secondsVal-1));
+        }
+        else{
+          seconds.text(secondsVal - 1);
+        }
+      }
     }, 1000);
   }
 });
